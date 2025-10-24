77899b536d318254efccbce4092a078e3508cf8a
PavanKumarBollu <bollupavankumar12@gmail.com>
2025-10-24 11:54:28 +0530

    F/O

---
+++ AdvPython/FileHandling.py
@@ -0,0 +1,24 @@
+"""
+File handling is the concept of the working with files like reading a file and writing to a file
+
+we can open a file or we can write something to that file
+
+
+the simple syntax for opening the file in program is follows
+
+file = open(file_path, mode)
+
+file -->  where the file is present in the system
+mode --> here when you work with file we have to say what do we want to do with that file like only read or writing
+something to that file ro read than write to that file ... etc
+
+"""
+
+
+# file = open("C:\\Users\\bpava\\Documents\\file.txt", 'r')
+#
+# if file:
+#     file_contents = file.read()
+#     print(file_contents, type(file_contents))
+
+
